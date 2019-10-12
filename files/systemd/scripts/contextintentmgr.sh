69790112d31f8ceb995525f22cfed5cab3c67986
tirthadeep.roy <tirthadeep.roy@lge.com>
2019-10-12 10:29:16 +0530

    [FIX] CIM node symlink is not created if if deleted
    :Release Notes:
    Fixed the issue of symlink not created
    
    :Detailed Notes:
    Fixed the issue of symlink not created
    
    :Testing Performed:
    Verified in local build
    
    :Issues Addressed:
    [PLAT-87053] CIM node symlink is not created if deleted
    
    Change-Id: I2b94be62522ac97a2a4a068b6829ff222da03239

--- files/systemd/scripts/contextintentmgr.sh
+++ files/systemd/scripts/contextintentmgr.sh
@@ -22,22 +22,22 @@ export CIM_NODES_DIR="/usr/palm/services/com.webos.service.contextintentmgr/.nod
 export CIM_USER_DIR="/var/cim/.nodered"
 
 # ensure that CIM user directories exist
-mkdir -p ${CIM_USER_DIR}
+mkdir -p $CIM_USER_DIR
 mkdir -p ${CIM_USER_DIR}/node_modules
 
 # set user directories permission level to nobody
-chown -R nobody:nogroup ${CIM_USER_DIR}
+chown -R nobody:nogroup $CIM_USER_DIR
 
-# # create symlink for default CIM nodes, needed for CIM runtime
+# create symlink for default CIM nodes, needed for CIM runtime
 for NODE_PATH in ${CIM_NODES_DIR}/node_modules/*; do
     destlink=${CIM_USER_DIR}/node_modules/$(basename "$NODE_PATH")
-    if [ ! \( -e "${destlink}" \) ]; then
-        ln -s ${NODE_PATH} $destlink
+    if [ ! -e $destlink ]; then
+        ln -s $NODE_PATH $destlink
     fi
 done
 
 # make a copy of package.json
 package=${CIM_USER_DIR}/package.json
-if [ ! \( -e "${package}" \) ]; then
-    cp ${CIM_NODES_DIR}/package.json ${CIM_USER_DIR}
+if [ ! -f $package ]; then
+    cp ${CIM_NODES_DIR}/package.json $CIM_USER_DIR
 fi
